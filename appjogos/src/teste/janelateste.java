aa0dba64027ad1c2961f807d2da33db0b7a8c725
Isabela Andrade <166050128+belaandradeb@users.noreply.github.com>
2025-09-30 09:52:34 -0300

    qqq

---
+++ appjogos/src/teste/janelateste.java
@@ -0,0 +1,11 @@
+package teste;
+
+import javax.swing.JFrame;
+
+public class janelateste {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame (title:"titulo da janela");
+        janela.setBounds(0,y,1920, height:1080;
+        janela.setvisible(b:true)
+    }
+}
